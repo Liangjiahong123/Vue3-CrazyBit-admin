a1bd94b028eb8565f96b2f5afa8e5b29c76fe91e
jiahong <2621762128@qq.com>
2023-09-28 01:21:28 +0800

    feat:login_dev

--- src/views/sys/login/c-cpns/LoginRecommend.vue
+++ src/views/sys/login/c-cpns/LoginRecommend.vue
@@ -6,7 +6,7 @@
     <div class="mt-14 text-white font-medium text-3xl">开箱即用的中后台管理系统</div>
     <div class="mt-5 text-white">输入您的个人详细信息开始使用！</div>
   </div>
-  <!-- login -->
+  <!-- 444 -->
 </template>
 
 <style lang="less" scoped></style>
