cf9cb9999351bd2fcd94cb44bc94ef07b659609c
jiahong <2621762128@qq.com>
2023-11-05 23:25:25 +0800

    updae:国际化配置

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -2,7 +2,9 @@ import { createI18n } from "vue-i18n";
 
 import type { App } from "vue";
 
+export let i18n: ReturnType<typeof createI18n>;
+
 export const setupI18n = (app: App) => {
-  const i18n = createI18n({});
+  i18n = createI18n({});
   app.use(i18n);
 };
