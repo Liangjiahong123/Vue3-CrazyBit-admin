03433a58bec03bd300241abe50719e4d70060dad
jiahong <2621762128@qq.com>
2023-11-15 00:00:45 +0800

    feat:Dropdown组件封装

--- src/hooks/settings/useAppSetting.ts
+++ src/hooks/settings/useAppSetting.ts
@@ -1,10 +1,11 @@
 import { useAppStore } from "@/stores/modules/app";
 import { ThemeEnum } from "@/enums/appEnum";
+import { storeToRefs } from "pinia";
 
 export const useAppSetting = () => {
   const appStore = useAppStore();
 
-  const getDarkMode = computed(() => appStore.getDarkMode);
+  const { getDarkMode } = storeToRefs(appStore);
 
   const setDarkMode = (mode: ThemeEnum) => appStore.setDarkMode(mode);
 
--- types/components.d.ts
+++ types/components.d.ts
@@ -11,6 +11,7 @@ declare module 'vue' {
     AppLogo: typeof import('./../src/components/Application/AppLogo.vue')['default']
     AppProvider: typeof import('./../src/components/Application/AppProvider.vue')['default']
     AppThemeToggle: typeof import('./../src/components/Application/AppThemeToggle.vue')['default']
+    Dropdown: typeof import('./../src/components/Dropdown/Dropdown.vue')['default']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElCol: typeof import('element-plus/es')['ElCol']
